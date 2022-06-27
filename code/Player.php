b45cb3fe811e4af65de5447082731e0afc47b623
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-27 12:20:34 +0200

    moved player.php to code folder

---
+++ code/Player.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+class Player
+{
+    private array $cards = [];
+    private bool $lost = false;
+
+    public function hit() {
+
+    }
+
+    public function surrender() {
+
+    }
+
+    public function getScore() {
+
+    }
+
+    public function hasLost() {
+
+    }
+
+
+}
