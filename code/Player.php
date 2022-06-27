820117d3ad5ba0759c1b808bea6e8d5bb8133ba7
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-27 16:31:47 +0200

    finished step 10
    I guess we will see if everything works on the next few steps

--- code/Player.php
+++ code/Player.php
@@ -9,25 +9,40 @@ class Player
 
     public function __construct(Deck $deck)
     {
+        // Starting hand for the player
         for ($i=0; $i<2; $i++) {
             $this->cards[] = $deck->drawCard();
         }
     }
 
-    public function hit() {
-
+    public function hit(Deck $deck) : void {
+        // `hit` should add a card to the player.
+        $this->cards[] += $deck->drawCard();
+        // If this brings him above 21, set the `lost` property to `true`.
+        // To count his score use the method `getScore` you wrote earlier.
+        // This method should expect the `$deck` variable as an argument from outside, to draw the card.
+        if ($this->getScore($this->cards) > 21){
+            $this->lost = true;
+        }
     }
 
-    public function surrender() {
-
+    public function surrender() : bool {
+        // This sets the property `lost` in the `player` instance to true.
+        return $this->lost = true;
     }
 
-    public function getScore() {
-
+    public function getScore(array $cards) : int {
+        // Loop over the cards and return the total value of that player
+        $score = 0;
+        foreach ($this->cards as $card){
+            $score += $card->getValue();
+        }
+        return $score;
     }
 
-    public function hasLost() {
-
+    public function hasLost() : bool {
+        // Return the bool of the lost property
+        return $this->lost;
     }
 
 
