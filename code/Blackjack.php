eef9dca5c0acc0602fde0a943a0b13f7dee0e3af
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-28 12:04:07 +0200

    creating a new dealer object

--- code/Blackjack.php
+++ code/Blackjack.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 class Blackjack
 {
     private Player $player;
-    private Player $dealer;
+    private Dealer $dealer;
     private Deck $deck;
 
     public function __construct()
@@ -13,7 +13,7 @@ class Blackjack
         $this->deck = new Deck();
         $this->deck->shuffle();
         $this->player = new Player($this->deck);
-        $this->dealer = new Player($this->deck);
+        $this->dealer = new Dealer($this->deck);
     }
 
     /**
