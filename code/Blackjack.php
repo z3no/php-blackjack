b8a89eaad63583651b61ae2936c6f9a3d7a39e99
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-28 10:11:16 +0200

    changed the order inside the constructor
    I changed the order of what's happening inside the blackjack constructor, don't know if this does anything, but when I echo'ed the session I got an error about the blackjack.php file, so I now first create a new deck, then shuffle it and I don't have an error anymore about that the deck is not set in the new player

--- code/Blackjack.php
+++ code/Blackjack.php
@@ -10,10 +10,10 @@ class Blackjack
 
     public function __construct()
     {
-        $this->player = new Player($this->deck);
-        $this->dealer = new Player($this->deck);
         $this->deck = new Deck();
         $this->deck->shuffle();
+        $this->player = new Player($this->deck);
+        $this->dealer = new Player($this->deck);
     }
 
     /**
