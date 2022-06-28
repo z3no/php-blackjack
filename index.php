bd236e07777e50247b133acfeb6fbe21f63a75d2
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-28 09:56:02 +0200

    index.php is created
    added session_start, the required files are added and we are creating a new blackjack object and putting the object in the session.

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 <?php
+
+declare(strict_types=1);
+
+/*If you don't write this line can't use $_Session global variable*/
+session_start();
+
+// Require all the files with the classes
+require 'code/Blackjack.php';
+require 'code/Card.php';
+require 'code/Deck.php';
+require 'code/Player.php';
+require 'code/Suit.php';
+
+// Create a new `Blackjack` object.
+// Put the `Blackjack` object in the session
+// Initializing a session variable
+$blackjack = new Blackjack();
+$_SESSION['blackjack'] = $blackjack;
+?>
