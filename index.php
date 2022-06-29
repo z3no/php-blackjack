9a62750ad87ab4f9c8a0f94f206e05a95a30c3be
Zeno Driesen <zeno.driesen@gmail.com>
2022-06-29 11:42:48 +0200

    buttons are doing stuff
    my buttons are working, not completely what I want, but reasonably happy with the result. Time to fine tune everything more.

--- index.php
+++ index.php
@@ -2,9 +2,6 @@
 
 declare(strict_types=1);
 
-/*If you don't write this line can't use $_Session global variable*/
-session_start();
-
 // Require all the files with the classes
 require './code/Blackjack.php';
 require './code/Card.php';
@@ -12,12 +9,78 @@ require './code/Deck.php';
 require './code/Player.php';
 require './code/Suit.php';
 
+// SET THE GAME
+
+/*If you don't write this line can't use $_Session global variable*/
+session_start();
+
 // Create a new `Blackjack` object.
 // Put the `Blackjack` object in the session
 // Initializing a session variable
-$blackjack = new Blackjack();
-$_SESSION['blackjack'] = $blackjack;
+//$blackjack = new Blackjack();
+//$_SESSION['blackjack'] = $blackjack;
 //echo $_SESSION['blackjack'];
+
+if (!isset($_SESSION['blackjack'])){
+    $_SESSION['blackjack'] = new Blackjack();
+}
+
+$blackJack = $_SESSION['blackjack'];
+
+$player = $blackJack->getPlayer();
+$dealer = $blackJack->getDealer();
+$deck = $blackJack->getDeck();
+
+$message = "";
+
+//Make those buttons work
+
+if (isset($_POST['action'])) {
+
+    // HIT BUTTON
+    if ($_POST['action'] === 'hit'){
+        $player->hit($deck);
+        if ($player->hasLost()){
+            $message = '<div class="alert alert-danger" role="alert">You lose! Reset to try again!</div>';
+        }
+    }
+
+    // STAND BUTTON
+    elseif ($_POST['action'] === 'stand'){
+        $dealer->hit($deck);
+        if (!$dealer->hasLost()) {
+            if ($player->getScore() < $dealer->getScore()) {
+                $message = '<div class="alert alert-danger" role="alert">The dealer wins! Reset for payback!</div>';
+            } elseif ($player->getScore() == $dealer->getScore()) {
+                $message = '<div class="alert alert-danger" role="alert">It is a tie! Too bad the House wins! Press reset to try again!</div>';
+            } else {
+                $message = '<div class="alert alert-success" role="alert">You are the winner! Reset the cards for another round!</div>';
+            }
+        } else {
+            $message = '<div class="alert alert-success" role="alert">You are the winner! Reset the cards for another round!</div>';
+        }
+    }
+
+    // SURRENDER BUTTON
+    elseif ($_POST['action'] === 'surrender'){
+        $message = '<div class="alert alert-danger" role="alert">You surrendered! Sometimes it is the better option. Please reset the cards.</div>';
+    }
+
+    // RESET GAME
+    elseif ($_POST['action'] === 'reset'){
+        //Unset Session
+        unset($_SESSION['blackjack']);
+        $_SESSION['blackjack'] = new Blackjack();
+
+        $blackJack = $_SESSION['blackjack'];
+
+        $player = $blackJack->getPlayer();
+        $dealer = $blackJack->getDealer();
+        $deck = $blackJack->getDeck();
+    }
+}
+
+
 
 ?>
 
@@ -36,12 +99,17 @@ $_SESSION['blackjack'] = $blackjack;
 <body>
 <div class="container px-4">
 
+    <div class="row">
+        <p><?php echo $message ?></p>
+    </div>
+
     <div class="row gx-5">
         <!-- PLAYER -->
         <div class="col">
             <h2>PLAYER</h2>
+            <h5>Total: <?php echo $player->getScore()?></h5>
             <?php
-                foreach($_SESSION['blackjack']->getPlayer()->getCards() AS $card) {
+                foreach($player->getCards() AS $card) {
                     echo '<span style="font-size: 8rem">' . $card->getUnicodeCharacter(true) . '</span>';
                 }
             ?>
@@ -50,8 +118,9 @@ $_SESSION['blackjack'] = $blackjack;
         <!-- DEALER -->
         <div class="col">
             <h2>DEALER</h2>
+            <h5>Total: <?php echo $dealer->getScore()?></h5>
             <?php
-            foreach($_SESSION['blackjack']->getDealer()->getCards() AS $card) {
+            foreach($dealer->getCards() AS $card) {
                 echo '<span style="font-size: 8rem">' . $card->getUnicodeCharacter(true) . '</span>';
             }
             ?>
@@ -60,9 +129,10 @@ $_SESSION['blackjack'] = $blackjack;
 
     <form method="post">
         <h2>What will be your next move?</h2>
-        <button type="submit" name="hit" value="hit">Hit</button>
-        <button type="submit" name="stand" value="stand">Stand</button>
-        <button type="submit" name="surrender" value="surrender">Surrender</button>
+        <button type="submit" name="action" value="hit" class="btn btn-dark">Hit</button>
+        <button type="submit" name="action" value="stand" class="btn btn-success">Stand</button>
+        <button type="submit" name="action" value="surrender" class="btn btn-danger">Surrender</button>
+        <button type="submit" name="action" value="reset" class="btn btn-danger">Reset</button>
     </form>
 </div>
 
